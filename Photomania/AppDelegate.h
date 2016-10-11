6a8d74c86788f0a0d5fc1b1a3bb0f5fa06c3f6fe
mdeaconu <mihai.deaconu@icloud.com>
2016-10-11 19:40:06 +0300

    initial app version

---
+++ Photomania/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photomania
+//
+//  Created by Mihai Deaconu on 11/10/16.
+//  Copyright © 2016 Mihai Deaconu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Photomania/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photomania
+//
+//  Created by Mihai Deaconu on 11/10/16.
+//  Copyright © 2016 Mihai Deaconu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
